027779b659d1b337b0aba4333e443b3e0579c9fb
Hannu <hannu.pelttari@gmail.com>
2019-05-28 16:58:37 +0300

    Tehtävä 2.4 valmis

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -27,40 +27,68 @@ const Course = ({ course })=> {
     <div>
       <Header course = {course.name} />
         <Content parts = {course.parts}/>
+        <Total parts = {course.parts} />
     </div>
   )
 }
 
+const Courses = ({ courses }) => 
+  courses.map(course => <Course key={course.id} course = {course}/>)
+
+const Total = ({ parts }) => {
+  return (
+    <div>yhteensä {parts.map(part => part.exercises).reduce( (s, p) => s + p )} tehtävää</div>
+  )
+}
 
 
 const App = () => {
-  const course = {
-    name: 'Half Stack -sovelluskehitys',
-    parts: [
-      {
-        name: 'Reactin perusteet',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Tiedonvälitys propseilla',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'Komponenttien tila',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack -sovelluskehitys',
+      id: 1,
+      parts: [
+        {
+          name: 'Reactin perusteet',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Tiedonvälitys propseilla',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'Komponenttien tila',
+          exercises: 14,
+          id: 3
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewaret',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  const total = course.parts.map(part => part.exercises).reduce( (s, p) => s + p )
+
 
   return (
     <div>
-      <Course course={course} />
-      <div>yhteensä {total} tehtävää</div>
+      <h1>Opetusohjelma</h1>
+      <Courses courses={courses} />
     </div>
   )
 }
