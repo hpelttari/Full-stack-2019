b2c01e24041d1ed00d84d87e47f1cc115bd676a5
Hannu <hannu.pelttari@gmail.com>
2019-07-02 17:32:23 +0300

    Tehtävä 2.9 valmis

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,14 +3,17 @@ import Person from './components/Person'
 
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: "040-1234567" 
-    }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+
+  const [condition, setCondition ] = useState('')
 
 
   const addPerson = (event) => {
@@ -31,6 +34,10 @@ const App = () => {
     }
   }
 
+  const handleConditionChange = (event) => {
+    setCondition(event.target.value)
+  }
+
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
@@ -39,9 +46,19 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+
+  let filtered = persons.filter(person => person.name.includes(condition))
+ 
+
   return (
     <div>
       <h2>Puhelinluettelo</h2>
+      
+      <div>Rajaa näytettäviä <input value={condition}
+          onChange={handleConditionChange}/></div>
+
+      <h2>Lisää uusi</h2>
+
       <form onSubmit={addPerson}>
         <div>Nimi: <input value={newName}
           onChange={handleNameChange} /></div>
@@ -52,7 +69,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numerot</h2>
-        {persons.map(person => <Person key={person.name} person={person}/>)}
+        {filtered.map(person => <Person key={person.name} person={person}/>)}
     </div>
   )
 
